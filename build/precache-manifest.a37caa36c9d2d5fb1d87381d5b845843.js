4f0e67153059bea90c9e26bd71e5a27e46b88cbc
Md. Rafikul Islam <contatwithrafik@gmail.com>
2022-08-04 13:02:29 +0600

    Start at 10-sep-2020

---
+++ build/precache-manifest.a37caa36c9d2d5fb1d87381d5b845843.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1e3c6d840e86992de963d98a848707c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d96e8b8ca9f84dbae20",
+    "url": "/static/css/main.5a39ce45.chunk.css"
+  },
+  {
+    "revision": "94a31309b52003f02c77",
+    "url": "/static/js/2.c4dfae9e.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/static/js/2.c4dfae9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d96e8b8ca9f84dbae20",
+    "url": "/static/js/main.ca4a756b.chunk.js"
+  },
+  {
+    "revision": "652a909d2c7614fc83c4",
+    "url": "/static/js/runtime-main.82c53c28.js"
+  }
+]);
